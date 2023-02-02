2e037b05fed8697cbdbe5c8d1a60dceb583b3ec2
Alex Merkert <alex.merkert@gmail.com>
2023-02-01 23:08:41 -0500

    tried to make puddles look cooler but.... i hate it

--- script/objects/puddle.js
+++ script/objects/puddle.js
@@ -2,6 +2,7 @@ const COLORS = require("../utils/colors");
 
 const MAX_PUDDLE_RADIUS = 50;
 const GROWTH_RATE = 0.1;
+const FRAME_CYCLE = 60;
 
 class Puddle {
   constructor(x, y, radius) {
@@ -9,6 +10,7 @@ class Puddle {
     this.y = y;
     this.radius = radius;
     this.totalGrowth = radius;
+    this.frame = 0;
   }
 
   drain() {
@@ -21,6 +23,7 @@ class Puddle {
       this.radius += GROWTH_RATE;
       this.totalGrowth += GROWTH_RATE;
     }
+    this.frame = (this.frame + 1) % FRAME_CYCLE;
   }
 
   draw(renderer) {
@@ -28,6 +31,30 @@ class Puddle {
       fillStyle: COLORS.WATER,
     }, () => {
       renderer.fillCircle(this.x, this.y, this.radius);
+      const count = 3;
+      const delta = FRAME_CYCLE / count;
+      let frameCalc = this.frame;
+      for (let i = 0; i < count; i += 1) {
+        frameCalc = (frameCalc + delta) % FRAME_CYCLE;
+        const adjust = 2 * Math.PI * frameCalc / FRAME_CYCLE;
+        renderer.isolatePath({
+          fillStyle: COLORS.WATER,
+        }, () => {
+          renderer.translate(this.x, this.y);
+          renderer.rotate(adjust);
+          // renderer.path({
+          //   strokeStyle: '#f00',
+          //   lineWidth: 2,
+          // }, () => {
+          //   renderer.strokePath([
+          //     { x: 0, y: 0 },
+          //     { x: this.radius * (0.75 + Math.cos(frameCalc / 20)), y: 0 }
+          //   ], { close: false });
+          // })
+          // renderer.scale(1.3, 0);
+          renderer.fillCircle(this.radius * 0.6, 0, this.radius / 2);
+        });
+      }
     });
   }
 }
